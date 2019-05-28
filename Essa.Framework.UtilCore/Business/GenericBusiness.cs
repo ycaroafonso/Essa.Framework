ab517eca94f6ebb35d136ae70702e445dac75fa6
Ycaro Afonso <ycaroafonso@outlook.com>
2019-05-28 13:22:54 -0400

    framework

---
+++ Essa.Framework.UtilCore/Business/GenericBusiness.cs
@@ -0,0 +1,21 @@
+﻿using Essa.Framework.UtilCore.Repository;
+
+namespace Essa.Framework.UtilCore.Business
+{
+    public interface IGenericBusiness
+    {
+
+    }
+
+    public abstract class GenericBusiness<TRepository, T> : IGenericBusiness
+        where TRepository : IGenericRepository<T>
+        where T : class
+    {
+        protected virtual TRepository Repository { get; private set; }
+
+        public GenericBusiness(TRepository repository)
+        {
+            Repository = repository;
+        }
+    }
+}
---
+++ Essa.Framework.WebCore/Helpers/JqGrid/GridSettings.cs
@@ -0,0 +1,81 @@
+﻿namespace Essa.Framework.WebCore.Helpers.JqGrid
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Json;
+    using System.Text;
+    using System.Web.Mvc;
+
+
+    [System.Web.Mvc.ModelBinder(typeof(GridModelBinder))]
+    public class GridSettings
+    {
+        public bool IsSearch { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public string SortColumn { get; set; }
+        public string SortOrder { get; set; }
+        public Filter Where { get; set; }
+    }
+
+    [DataContract]
+    public class Filter
+    {
+        [DataMember]
+        public string groupOp { get; set; }
+        [DataMember]
+        public Rule[] rules { get; set; }
+
+        public static Filter Create(string jsonData)
+        {
+            try
+            {
+                var serializer = new DataContractJsonSerializer(typeof(Filter));
+                System.IO.StringReader reader = new System.IO.StringReader(jsonData);
+                System.IO.MemoryStream ms = new System.IO.MemoryStream(Encoding.UTF8.GetBytes(jsonData));
+                return serializer.ReadObject(ms) as Filter;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+
+    [DataContract]
+    public class Rule
+    {
+        [DataMember]
+        public string field { get; set; }
+        [DataMember]
+        public string op { get; set; }
+        [DataMember]
+        public string data { get; set; }
+    }
+
+    public class GridModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext,
+                                ModelBindingContext bindingContext)
+        {
+            try
+            {
+
+                var request = controllerContext.HttpContext.Request;
+                return new GridSettings
+                {
+                    IsSearch = bool.Parse(request["_search"] ?? "false"),
+                    PageIndex = int.Parse(request["page"] ?? "1"),
+                    PageSize = int.Parse(request["rows"] ?? "10"),
+                    SortColumn = request["sidx"] ?? "",
+                    SortOrder = request["sord"] ?? "asc",
+                    Where = Filter.Create(request["filters"] ?? "")
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
