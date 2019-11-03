abfeb9159e22ce6d8ac277115456b1cb5b79693b
Ycaro Afonso <ycaroafonso@gmail.com>
2019-11-03 09:15:08 -0400

    .

--- Essa.Framework.Util/Properties/AssemblyInfo.cs
+++ Essa.Framework.Util/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Essa.Framework.Util")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Framework util para qualquer project em .NET Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Essa Tecnologia")]
 [assembly: AssemblyProduct("Essa.Framework.Util")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
