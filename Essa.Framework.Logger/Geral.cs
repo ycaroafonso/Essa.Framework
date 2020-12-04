c80620f65d1debc3ace9a069d79fd1c006ed0d57
Ycaro Afonso <ycaroafonso@gmail.com>
2020-12-04 02:11:28 -0400

    .

--- Essa.Framework.Logger/Geral.cs
+++ Essa.Framework.Logger/Geral.cs
@@ -17,8 +17,22 @@
 .MinimumLevel.Debug()
 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
 .Enrich.FromLogContext()
+.Enrich.WithProperty("Sistema", nomePrograma)
 .WriteTo.Console()
-.WriteTo.File($"{arquivolog}log_{nomePrograma}_", rollingInterval: RollingInterval.Day)
+.WriteTo.File($"{arquivolog}log_{nomePrograma}_.txt", rollingInterval: RollingInterval.Day)
+.CreateLogger();
+        }
+
+
+        public static Serilog.Core.Logger LogMongoDb(string nomePrograma = "", string diretoriolog = "")
+        {
+            return new LoggerConfiguration()
+.MinimumLevel.Debug()
+.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+.Enrich.FromLogContext()
+.Enrich.WithProperty("Sistema", nomePrograma)
+.WriteTo.Console()
+.WriteTo.MongoDB("mongodb://localhost/logs")
 .CreateLogger();
         }
     }
